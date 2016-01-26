3293d595140f6eaf5a64c6e595dc72ef43045a28
León Castillejos <leoncastillejos@protonmail.com>
2016-01-26 11:55:01 +0100

    reads the API correctly

--- src/sonar/sonar_server/app/controllers/application_controller.rb
+++ src/sonar/sonar_server/app/controllers/application_controller.rb
@@ -2,16 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  def test
-      render text: "this is a test"
-  end
   def jsontest
-      require 'json'
-      # require 'awesome_print'
-      # require 'httparty'
-      t = ap JSON.parse HTTParty.get('http://127.0.0.1:4963/cpu').response.body
-      # jsonroute = "http://127.0.0.1:4963/cpu"
-      # t = JSON.parse jsonroute
-      render text: "CPU = #{t}"
+    url = 'http://localhost:4963/cpu'
+    response = RestClient.get(url)
+    hash = JSON.parse(response)
+    render text: "CPU usage is at #{hash["load_percentage"]}%"
   end
 end
