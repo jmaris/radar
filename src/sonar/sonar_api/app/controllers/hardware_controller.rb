a5336ded4b3eeba36d205c588179abb9891be017
León Castillejos <leoncastillejos@protonmail.com>
2016-02-02 12:00:55 +0100

    API changes

--- src/sonar/sonar_api/app/controllers/hardware_controller.rb
+++ src/sonar/sonar_api/app/controllers/hardware_controller.rb
@@ -1,36 +1,42 @@
 class HardwareController < ApplicationController # logic goes in this hardware_controller.rb file
     def status
         render json: {  load: {
-                            "cpu_percentage":   (Sys::CPU.load_avg.first*100 / Sys::CPU.processors.count).round(2), #averaged to the last minute
-                            "cpu_load_average": Sys::CPU.load_avg,
-                            "ram":              50456456,
-                            "swap":             65435,
-                            "storage_bytes":    Sys::Filesystem.stat("/").bytes_free
+                            "cpu_percentage":   (Sys::CPU.load_avg.first*100 / Sys::CPU.processors.count).round(2), #cpu load averaged to the last minute
+                            "cpu_load_average": Sys::CPU.load_avg, # standard UNIX load average
+                            "ram_bytes":        Vmstat.memory.active_bytes,
+                            # "swap_bytes":       65435, # not yet implemented
+                            "storage_bytes":    (Sys::Filesystem.stat("/").blocks * Sys::Filesystem.stat("/").block_size) - Sys::Filesystem.stat("/").bytes_free, # this gives the used bytes
+                            "uptime_seconds":   Sys::Uptime.seconds
                             },
-                            io: {
-                                storage: {
-                                    "in":   34.5,
-                                    "out":  44
-                                    },
-                                net: {
-                                    "in":   50,
-                                    "out":  75
-                                    }
-                                    },
+                            # io: { # not yet implemented
+                            #     storage: {
+                            #         "in":   34.5,
+                            #         "out":  44
+                            #         },
+                            #     net: {
+                            #         "in":   50,
+                            #         "out":  75
+                            #         }
+                            #         },
                             sysinfo: {
                                 "hostname": Socket.gethostname,
-                                        cpu: {
-                                            "CPU model": "Intel i7 5694K",
-                                            "Number of cores": Sys::CPU.processors.count
-                                            },
-                                        ram: {
-                                            "Total RAM": 17179869184,
-                                            "Total swap space": 17179869184
-                                            },
-                                        storage: {
-                                            "Total storage space": (Sys::Filesystem.stat("/").blocks * Sys::Filesystem.stat("/").block_size)
-                                        }
-                                        }
+                                    os: {
+                                        "Family": Sys::Uname.sysname,
+                                        "Release": Sys::Uname.release
+                                        },
+                                    cpu: {
+                                        "CPU model": Sys::CPU.processors.first.model_name,
+                                        "Number of cores": Sys::CPU.processors.count,
+                                        "Architecture": Sys::Uname.machine
+                                        },
+                                    ram: {
+                                        "Total RAM bytes": 17179869184,
+                                        "Total swap bytes": 17179869184
+                                        },
+                                    storage: {
+                                        "Total storage bytes": (Sys::Filesystem.stat("/").blocks * Sys::Filesystem.stat("/").block_size)
                                     }
-                                end
-                            end
+                                }
+                            }
+        end
+    end
