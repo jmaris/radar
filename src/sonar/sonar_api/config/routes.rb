5048dbe41d2025267bcf90a9d2caea4dc5eef27a
León Castillejos <leoncastillejos@protonmail.com>
2016-02-16 18:25:10 +0100

    Removed code duplication in hardware_controller

--- src/sonar/sonar_api/config/routes.rb
+++ src/sonar/sonar_api/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#root'
 
-  get 'sonar_api_v1'          => 'hardware#status'
+  # get 'sonar_api_v1'          => 'hardware#status'
   get 'sonar_api_v1/live'     => 'hardware#live'
   get 'sonar_api_v1/sysinfo'  => 'hardware#sysinfo'
   
