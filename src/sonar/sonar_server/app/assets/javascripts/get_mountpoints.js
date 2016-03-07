bd79d86f06f1ebd6b07f157a28c7fb25751a125b
Leon Castillejos <leoncastillejos@protonmail.com>
2016-03-07 12:25:37 +0100

    this code does not work yet

--- src/sonar/sonar_server/app/assets/javascripts/get_mountpoints.js
+++ src/sonar/sonar_server/app/assets/javascripts/get_mountpoints.js
@@ -5,9 +5,24 @@
     {
       if (xhttp.readyState == 4 && xhttp.status == 200)
       {
-        document.getElementById("mountpoint_list").innerHTML = xhttp.responseText;
-        // document.getElementById("storage_alert_path").options.length = 0;
-        document.forms["storage_alert_form"].elements["storage_alert[path]"].options.length = 0;
+        ajaxresponse = xhttp.responseText;
+        ajaxresponse_json = JSON.parse(ajaxresponse)
+        // select_name = "storage_alert[path]";
+        // form_name = "storage_alert_form";
+        var select = document.getElementById("storage_alert_path")
+        document.getElementById("mountpoint_list").innerHTML = ajaxresponse;
+        document.getElementById("storage_alert_path").options.length = 0;
+        // document.forms[form_name].elements["storage_alert[path]"].options.length = 0;
+//         
+//         var master=document.myform.master
+//         for (i=0; i<somevariable; i++){
+//           master.options[master.options.length]=new Option(...)
+//         }
+//      //
+        var select=document.getElementById("storage_alert_path")
+        for (i=0; i<ajaxresponse.length; i++){
+          select.options[select.options.length] = new ajaxresponse_json.mountpoints[i]
+        }
       };
     };
     xhttp.open("GET", "../machine_mountpoints/"+machine_id, true);
