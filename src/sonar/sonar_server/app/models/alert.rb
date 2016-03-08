d0414fa1c8455b01469ffe85474ab76f8aa40623
Leon Castillejos <leoncastillejos@protonmail.com>
2016-03-08 13:46:29 +0100

    fixed validation with check delays

--- src/sonar/sonar_server/app/models/alert.rb
+++ src/sonar/sonar_server/app/models/alert.rb
@@ -4,7 +4,7 @@ class Alert < ActiveRecord::Base
 
     belongs_to		:machine
 
-    validates   	:check_interval, presence: true, numericality: { only_integer: true }
+    validates   	:check_interval, presence: true, numericality: { only_integer: true }, inclusion: {in: 1..100}
     validates   	:addressee, presence: true
     validate    	:machine
 
