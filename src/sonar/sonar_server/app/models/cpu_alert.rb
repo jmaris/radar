23bdcbe6145257d7a80c1cc6cd3aeeca2a9261d2
Leon Castillejos <leoncastillejos@protonmail.com>
2016-03-21 11:42:48 +0100

    references #58 cpu_alerts is fixed, now I need to do the same on the rest of the alerts

--- src/sonar/sonar_server/app/models/cpu_alert.rb
+++ src/sonar/sonar_server/app/models/cpu_alert.rb
@@ -1,21 +1,26 @@
 class CpuAlert < ActiveRecord::Base
-	acts_as		:alert
+  acts_as:alert
 
-	validates :threshold, presence: true, numericality: true, inclusion: { in: 0..100 }
+  validates :threshold, presence: true, numericality: true, inclusion: { in: 0..100 }
 
-	private
+  private
 
-	def self.is_higher(cpu_alert_id)
-		machine 	= Machine.find(Alert.where(actable_type: "CpuAlert", actable_id: cpu_alert_id).first.machine_id)
+  def self.is_higher(cpu_alert_id)
+    machine   = Machine.find(Alert.where(actable_type: "CpuAlert", actable_id: cpu_alert_id).first.machine_id)
 
-		live_cpu = CpuMetric.where(machine_id: 4).last.cpu
-		cpu_alert = CpuAlert.find(cpu_alert_id)
-		threshold = cpu_alert.threshold
+    # live_cpu = CpuMetric.where(machine_id: 4).last.cpu
+    cpu_alert = CpuAlert.find(cpu_alert_id)
+    threshold = cpu_alert.threshold
 
-		if live_cpu > threshold
-		  true
-		else
-		  false
-		end
-	end
+    cycles = cpu_alert.duration / cpu_alert.check_interval
+
+    for i in 0..(cycles - 1)
+      if CpuMetric.where(machine_id: machine.id).last(cycles)[i].cpu < threshold
+        return false
+      end
+      if i == (cycles - 1)
+        return true
+      end
+    end
+  end
 end
