a2f3b6ef39ee7aa8865d6a9a5782ecb4624798e6
Leon Castillejos <leoncastillejos@protonmail.com>
2016-05-11 13:55:54 +0200

    created directories

--- doc/web-autodeploy.bash
+++ doc/web-autodeploy.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-gem install bundler
+cd /home/rails/sonar/src/sonar/sonar_server/bin/
+RAILS_ENV=production /home/rails/sonar/src/sonar/sonar_server/bin/delayed_job stop
+cd /root/
 service unicorn stop
 service nginx stop
 echo "===== SHUT DOWN SERVICES ====="
@@ -8,6 +10,8 @@ rm -rf /home/rails/sonar
 echo "===== DELETED OLD GIT REPO ====="
 git clone https://github.com/leoncastillejos/sonar.git
 echo "===== CLONED GIT REPO ====="
+gem install bundler
+echo "===== INSTALLED BUNDLER ====="
 cp /home/rails/rails_project/config/database.yml /home/rails/sonar/src/sonar/sonar_server/config/database.yml
 echo "===== COPIED DATABASE CONFIGURATION FILES ====="
 sed -i -e 's/rails_project/sonar/g' /home/rails/sonar/src/sonar/sonar_server/config/database.yml
@@ -21,10 +25,14 @@ RAILS_ENV=production rake db:migrate
 echo "===== MIGRATED DATABASE ====="
 RAILS_ENV=production rake assets:precompile
 echo "===== PRECOMPILED ASSETS ====="
+mkdir /home/rails/sonar/src/sonar/sonar_server/tmp
+mkdir /home/rails/sonar/src/sonar/sonar_server/tmp/pids
+echo "===== CREATED DIRECTORIES ====="
 chown -R rails: /home/rails/sonar
 echo "===== CHANGED OWNERSHIP OF RAILS PROJECT FILES ====="
 service unicorn restart
 service nginx restart
 echo "===== RESTARTED SERVICES ====="
+cd /home/rails/sonar/src/sonar/sonar_server/bin/
 RAILS_ENV=production /home/rails/sonar/src/sonar/sonar_server/bin/delayed_job start
 echo "===== STARTED DELAYED JOB ====="
